940448ad7334b30f4d6679fe55b9c06c87807cf7
melekdogan <melekdogand@gmail.com>
2022-08-13 21:58:08 +0300

    Fixed Connection Limit Error

--- BackgroundJobs/Concrete/SendMailService.cs
+++ BackgroundJobs/Concrete/SendMailService.cs
@@ -16,11 +16,11 @@ namespace BackgroundJobs.Concrete
             mailMessage.Subject = "Password Information";
             mailMessage.Body = new TextPart("plain")
             {
-                Text = $"Hello {Firstname}! \n Your password here: {password}"
+                Text = $"Hello {Firstname}!\n Your password here: {password}"
             };
             using (var smtpClient = new SmtpClient())
             {
-                smtpClient.Connect("smtp.office365.com", 587,SecureSocketOptions.StartTls);
+                smtpClient.Connect("smtp-mail.outlook.com", 587, SecureSocketOptions.StartTlsWhenAvailable);
                 smtpClient.Authenticate("forprojectmd@outlook.com", "78m93o74a25m");
                 smtpClient.Send(mailMessage);
                 smtpClient.Disconnect(true);
